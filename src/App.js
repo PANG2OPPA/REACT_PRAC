6fd979fcc668029525c1bc71df4fd6535e9571fb
PANG2OPPA <khbkhb85098509@gmail.com>
2023-10-31 20:46:10 +0900

    10월 31일 수업자료입니다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import { Map } from 'react-kakao-maps-sdk';
-import Location from './location';
+import { useState } from 'react';
+import './App.css';
 
 const App = () => {
+
+    let post = 'REACT_PRAC';
+    let [title, setTitle] = useState(['리액트 공부하는 법', '리액트로 API 적용하는 법', '리액트 독학']);
+    let [like, setLike] = useState(0);
+    
+
 	return (
-    <Location/>  
+    <div className="App">
+        <div className="black-nav">
+        <h4>{post}</h4>
+        </div>
+        <button onClick={()=>{
+            let copy1 = {...title}; 
+            copy1.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1); 
+            setTitle(title);}}>가나다순</button>
+
+        <button onClick={()=>{
+            let copy = [...title]; 
+            copy[0] = '프론트 공부하는 법';
+            setTitle(copy);
+            }}>글 수정</button>
+        <div className="list">
+            <h4>{title[0]} <span onClick={()=>{setLike(like+1) }}>👍</span>{like}</h4>
+            <p>10월 31일 발행</p>
+        </div>
+        <div className="list">
+            <h4>{title[1]}</h4>
+            <p>10월 31일 발행</p>
+        </div>
+        
+    </div>
     );
 };
 
